c8e0bbdadd411585da6dcc7ccb5d5d6ef81798bc
CHARAN G <charang2003@gmail.com>
2024-06-25 21:18:44 +0530

    completed get_state function

--- agent.py
+++ agent.py
@@ -18,7 +18,49 @@ class Agent:
         #model,trainer
     
     def get_state(self, game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20,head.y)
+        point_r = Point(head.x + 20,head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y - 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u= game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+        
+        state = [
+            # danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+            
+            #danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+            
+            #danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)) ,
+            
+            #move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            #food location
+            game.food.x < game.head.x , #food left
+            game.food.x > game.head.x , #food right
+            game.food.y < game.head.y , #food up
+            game.food.y < game.head.y  #food down
+        ]
+        return np.array(state, dtype=int)
     
     def remember(self, state, action, reward, next_state, done):
         pass

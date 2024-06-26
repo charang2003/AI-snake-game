2ccc53573f5707002eafb2dfc6db50d9f7d7f533
CHARAN G <charang2003@gmail.com>
2024-06-26 22:06:02 +0530

    completed with agent.py

--- agent.py
+++ agent.py
@@ -84,7 +84,18 @@ class Agent:
         self.trainer.train_step(state, action, reward, next_state, done)
     
     def get_action(self, state):
-        pass
+        #random moves: tradeoff exploration/ exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0,2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1 
+        return final_move
     
 def train():
     plot_scores = []
